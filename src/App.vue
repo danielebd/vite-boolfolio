b9291b1dd50a20057d4ee676c8d860070a98f642
danielebd <danielebalebalestro@gmail.com>
2023-05-25 17:04:59 +0200

    chiamata axios dei projects in console

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <script >
+import axios from 'axios';
 import HelloApp from './components/HelloApp.vue';
 export default {
   name: 'App',
+  data() {
+    return {
+      apiUrlBase: 'http://127.0.0.1:8000/api',
+      apiUrl: {
+        projects: '/projects'
+      },
+      projects: []
+    }
+  },
   components: {
     HelloApp
+  },
+  methods: {
+    getProjects() {
+      //concatenazione stringhe
+      axios.get(this.apiUrlBase + this.apiUrl.projects).then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log('error');
+      })
+    }
+  },
+  created() {
+    this.getProjects()
   }
 }
 </script>
@@ -12,7 +36,4 @@ export default {
   <HelloApp />
 </template>
 
-<style lang="scss" scoped>
-
-
-</style>
+<style lang="scss" scoped></style>
